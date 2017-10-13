18e5786916a9f4491e74b1a75b86f03866d03de4
Alexandr <ar.kraushkin@gmail.com>
2017-10-13 22:20:28 +0300

    bla

---
+++ Desktop/git_exercise/test.py
@@ -0,0 +1,9 @@
+number=42
+while True:
+12323
+    otvet=input("Вводи сука код: ")
+    if otvet==number: print("break")
+
+
+
+
---
+++ test3.py
@@ -0,0 +1,12 @@
+number=42
+while True:
+4354354
+    werwerwerwerewrewrewrew
+
+    otvet=input("Вводи сука код: ")
+    if otvet==number: print("break")
+
+
+
+
+dfdfgdfg
